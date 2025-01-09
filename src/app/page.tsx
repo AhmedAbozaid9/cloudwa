a888e1e3081079c14885f788137eec896b68ab2b
Ahmed <ahmedabozaid.dev@gmai.com>
2025-01-09 06:25:23 +0200

    adds chat components structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import MainChat from "@/components/Chat/MainChat";
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="">hello world</div>;
+  return (
+    <div className="">
+      <MainChat />
+    </div>
+  );
 }
---
+++ src/components/Chat/MainChat.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Message from "./Message";
+import SendMessage from "./SendMessage";
+
+const MainChat = () => {
+  return (
+    <div>
+      <Message />
+      <SendMessage />
+    </div>
+  );
+};
+
+export default MainChat;
